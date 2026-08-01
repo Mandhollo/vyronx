b67be0b129e7ba1c9006209083bc2f9fa819f35d
Mandhollo <mandhollo@hotmail.com>
2026-08-01 18:40:42 -0300

    feat: add new owner wallet to admin access
    - Ownership transferred to 0x7761...2f7c7
    - Both wallets have admin access (deployer + new owner)

--- web/lib/admin-wallets.ts
+++ web/lib/admin-wallets.ts
@@ -4,7 +4,8 @@
  * To add more, just append to this array.
  */
 export const ADMIN_WALLETS: readonly string[] = [
-  '0xd7a8484fd713d28870fcd4ad198fab9e3ffdedb1', // Anderson
+  '0xd7a8484fd713d28870fcd4ad198fab9e3ffdedb1', // Anderson (deployer)
+  '0x77619322427f006b14DA3Dbb25F9eb420372f7c7', // Anderson (new owner)
 ].map((a) => a.toLowerCase());
 
 export function isAdminWallet(address?: string): boolean {
